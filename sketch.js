4eedecf98602ae84525854345f12c34421e15ccc
VaishnaviManagoli <72454073+VaishnaviManagoli@users.noreply.github.com>
2020-12-15 18:44:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+var database, position;
+var player,form,game;
+var gameState = 0;
+var playerCount;
+
+function setup(){
+    createCanvas(400,400);
+    database = firebase.database();
+   game = new Game();
+   game.getState();
+   game.start();
+   
+}
+
+function draw(){
+    background("white");
+    
+  
+}
+
